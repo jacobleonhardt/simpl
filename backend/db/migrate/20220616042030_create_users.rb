6722df4a5c1ec3d678ed3a1ad43d4f078d5898e7
Jacob Leonhardt <jacob.leonhardt649@gmail.com>
2022-06-15 23:32:34 -0500

    run migration for users model

--- backend/db/migrate/20220616042030_create_users.rb
+++ backend/db/migrate/20220616042030_create_users.rb
@@ -1,10 +1,9 @@
 class CreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
-      t.integer :id
       t.string :name, null: false
       t.string :email, null: false
-      t.string :password
+      t.string :password, null: false
       t.integer :highest_lesson_completed
 
       t.timestamps
