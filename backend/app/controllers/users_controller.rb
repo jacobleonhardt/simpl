7f6efd33b62a221e2ac55cd9f7ad3ab8b272df2a
Jacob Leonhardt <jacob.leonhardt649@gmail.com>
2022-06-16 00:29:22 -0500

    begin work on users_controller methods

--- backend/app/controllers/users_controller.rb
+++ backend/app/controllers/users_controller.rb
@@ -1,2 +1,24 @@
 class UsersController < ApplicationController
+
+  # before_action :require_user_logged_in!, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user ||= @current_user
+  end
+
+  def new
+    @user = User.new
+  end
+
+
+  private
+
+  def user_params
+    params.require_user(:user).permit(:name, :email, :password_digest)
+  end
+
 end
