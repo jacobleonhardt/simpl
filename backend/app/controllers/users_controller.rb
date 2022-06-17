483f62f68c70fb62d470b3ac35c5fb68f60d2fcf
Jacob Leonhardt <jacob.leonhardt649@gmail.com>
2022-06-16 22:16:22 -0500

    update user controller methods

--- backend/app/controllers/users_controller.rb
+++ backend/app/controllers/users_controller.rb
@@ -14,8 +14,43 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to @user
+    else
+      render "new"
+    end
+  end
+
+  def edit
+    @user = User.find(user_id)
+  end
+
+  def update
+    @user = User.find(user_id)
+    if @user.update(user_params)
+      redirect_to @user
+    else
+      render "edit"
+    end
+  end
+
+  def destroy
+    @user = User.find(user_id)
+    @user.destroy
+    session[:user_id] = nil
+    redirect_to root_path
+  end
+
 
   private
+
+  def user_id
+    params.require_user(:user).permit(:id)
+  end
 
   def user_params
     params.require_user(:user).permit(:name, :email, :password_digest)
