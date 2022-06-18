98bcd6b1982fded37253cfac84f526771e7c2933
Jacob Leonhardt <jacob.leonhardt649@gmail.com>
2022-06-18 00:11:44 -0500

    begin working on connecting react to rails

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -1,13 +1,8 @@
-User.create([{
+User.create({
   name: 'Playswith Squirrels',
   email: 'test@gmail.com',
   password_digest: 'testing123',
   highest_lesson_completed: 1,
-},{
-  name: 'Test',
-  email: 'BAM@test.com',
-  password_digest: 'it_worked',
-  highest_lesson_completed: 50,
-}])
+})
 
 p "Data Seeded"
